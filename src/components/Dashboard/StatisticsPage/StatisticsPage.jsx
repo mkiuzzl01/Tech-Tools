c6402ba49bf2ea965650ee100fe04a231f4cb68f
mkiuzzal01 <mkiuzzal07@gmail.com>
2024-06-12 15:26:32 +0600

    admin statitics impliment on pi chart in admin dashborad

--- src/components/Dashboard/StatisticsPage/StatisticsPage.jsx
+++ src/components/Dashboard/StatisticsPage/StatisticsPage.jsx
@@ -1,10 +1,76 @@
+import { useQuery } from "@tanstack/react-query";
+import { PieChart, Pie, Cell, Legend } from "recharts";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Loading from "../../Shared/Loading/Loading";
+
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
 
 const StatisticsPage = () => {
+  const axiosSecure = useAxiosSecure();
+  const { data: Stats = [], isLoading } = useQuery({
+    queryKey: ["Statistics"],
+    queryFn: async () => {
+      const { data } = await axiosSecure.get("/Admin-Statistics");
+      return data;
+    },
+  });
+  //   console.log(Stats);
+
+  if (isLoading) return <Loading></Loading>;
+
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
     return (
-        <div>
-            <h1>this is Statistics Page.</h1>
-        </div>
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
     );
+  };
+
+  return (
+    <div>
+      <h1 className="text-center py-10 text-3xl font-bold">Data Statistics</h1>
+      <div className="flex justify-center">
+        <PieChart width={400} height={400}>
+          <Pie
+            data={Stats}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius={80}
+            fill="#8884d8"
+            dataKey="value"
+          >
+            {Stats.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+          <Legend></Legend>
+        </PieChart>
+      </div>
+    </div>
+  );
 };
 
 export default StatisticsPage;
